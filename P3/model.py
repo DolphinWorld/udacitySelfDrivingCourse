17b7473ff9a7aa8df3fb1f23bbc583ab4794faf0
Su <sujack@amazon.com>
2017-04-14 21:17:37 -0700

    Added dropped out

--- P3/model.py
+++ P3/model.py
@@ -47,7 +47,6 @@ model = Sequential()
 model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((55,25), (0,0))))
 
-#model.add(Dropout(0.20))
 model.add(Convolution2D(24,5,5, subsample=(2,2), activation="relu"))
 model.add(Convolution2D(36,5,5, subsample=(2,2), activation="relu"))
 model.add(Convolution2D(48,5,5, subsample=(2,2), activation="relu"))
@@ -57,10 +56,11 @@ model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(50))
 model.add(Dense(10))
+model.add(Dropout(0.20))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.3, shuffle=True, nb_epoch=5)
+model.fit(X_train, y_train, validation_split=0.3, shuffle=True, nb_epoch=10)
 
 model.save('model.h5')
 
